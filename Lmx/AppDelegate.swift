81d427b0353a6f957d3cfd31ec60fd496e4c2baf
limingxia <limingxia@bocservices.com.cn>
2020-06-11 16:18:30 +0800

    添加横竖屏控制切换控制

--- Lmx/AppDelegate.swift
+++ Lmx/AppDelegate.swift
@@ -10,7 +10,19 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    // 是否允许横竖屏切换
+    var blockRotation: Bool = false
+    
+    /* 通过blockRotation字段动态设置某个页面开启横竖屏 */
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if blockRotation {
+            return .allButUpsideDown
+        }
+        
+        return .portrait
+    }
+    
 }
 
--- Lmx/ViewControllers/SJVideoPlayerViewController.swift
+++ Lmx/ViewControllers/SJVideoPlayerViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 import SJVideoPlayer
 
 class SJVideoPlayerViewController: UIViewController {
-    var player: SJVideoPlayer!
+    var player: SJVideoPlayer = SJVideoPlayer()
     var url = ""
+    let appDelegate:AppDelegate = (UIApplication.shared.delegate) as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        player = SJVideoPlayer()
         
         view.addSubview(player.view)
         player.view.translatesAutoresizingMaskIntoConstraints = false
@@ -31,7 +30,56 @@ class SJVideoPlayerViewController: UIViewController {
         let asset = SJVideoPlayerURLAsset(url: videoPlayerUrl)
         
         player.urlAsset = asset
-        
+        //调整设备亮度
+//        player.deviceVolumeAndBrightnessManager.brightness = 1.0
+        //调整设备声音
 //        player.deviceVolumeAndBrightnessManager.volume = 1.0
+        //是否竖屏时隐藏返回按钮
+        player.defaultEdgeControlLayer.isHiddenBackButtonWhenOrientationIsPortrait = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        appDelegate.blockRotation = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        player.vc_viewDidAppear()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        appDelegate.blockRotation = false
+        player.vc_viewWillDisappear()
+        
+        //判断退出时是否是横屏
+        if UIApplication.shared.statusBarOrientation.isLandscape {
+            //是横屏让变回竖屏
+            setNewOrientation(fullScreen: false)
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        player.vc_viewDidDisappear()
+    }
+    
+    //横竖屏设置
+    func setNewOrientation(fullScreen: Bool) {
+        if fullScreen { //横屏
+            let resetOrientationTargert = NSNumber(integerLiteral: UIInterfaceOrientation.unknown.rawValue)
+            UIDevice.current.setValue(resetOrientationTargert, forKey: "orientation")
+            
+            let orientationTarget = NSNumber(integerLiteral: UIInterfaceOrientation.landscapeLeft.rawValue)
+            UIDevice.current.setValue(orientationTarget, forKey: "orientation")
+            
+        } else { //竖屏
+            let resetOrientationTargert = NSNumber(integerLiteral: UIInterfaceOrientation.unknown.rawValue)
+            UIDevice.current.setValue(resetOrientationTargert, forKey: "orientation")
+            
+            let orientationTarget = NSNumber(integerLiteral: UIInterfaceOrientation.portrait.rawValue)
+            UIDevice.current.setValue(orientationTarget, forKey: "orientation")
+        }
     }
 }
